677fd3f1f2f8904594a39333ad7108ceb1ee7e44
Gabriela Araujo <gabyara237@gmail.com>
2025-12-13 00:28:45 -0800

    feat: add new artwork form and create artwork route

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user.ejs');
-const Artwork = require('../models/artwork.ejs')
+const User = require('../models/user.js');
+const {Artwork, ART_TYPES} = require('../models/artwork.js')
 
+router.get('/', (req,res)=>{
+    res.render('./artworks/index.ejs');
+})
+
+router.get('/new', (req,res)=>{
+    res.render('artworks/new.ejs',{ART_TYPES});
+})
+
+router.post('/', async (req,res) =>{
+    try{
+        const newArtwork = new Artwork(req.body);
+        newArtwork.owner = req.session.user._id;
+
+       await newArtwork.save();
+       res.redirect('/artworks');
+
+    }catch (error){
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 module.exports = router;

568844aa3919ed314f5a9540650ba50e8d7687b3
Gabriela Araujo <gabyara237@gmail.com>
2025-12-12 16:09:29 -0800

    feat: add artworks controller

---
+++ controllers/artworks.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.ejs');
+const Artwork = require('../models/artwork.ejs')
+
+
+module.exports = router;

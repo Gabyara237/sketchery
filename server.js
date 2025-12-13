1745c7ead7c5aa93c29529b953cef545ca578970
Gabriela Araujo <gabyara237@gmail.com>
2025-12-12 16:37:32 -0800

    feat: register artworks controller

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const artworksController = require('./controllers/artworks.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -36,6 +37,8 @@ app.get('/', (req, res) => {
 
 
 app.use('/auth', authController);
+app.use('/artworks', artworksController);
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);

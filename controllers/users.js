37cbaf127c77f1b31901d8db37ab987303b8d873
Gabriela Araujo <gabyara237@gmail.com>
2025-12-16 15:07:30 -0800

    feat: display user profile with their artworks

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const User = require('../models/user.js');
+const {Artwork} = require('../models/artwork.js');
 
-
-router.get('/',(req,res)=>{
-
-    res.render("users/index.ejs");
+router.get('/:userId',async(req,res)=>{
+    const userProfile = await User.findById(req.params.userId);
+    const allArtworks = await Artwork.find({owner:req.params.userId});
+    res.render("users/show.ejs", {allArtworks, userProfile});
 })
 
 

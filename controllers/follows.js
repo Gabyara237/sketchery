e1c53699c7a2cd0b7f9f193134eb2aabe7d97e91
Gabriela Araujo <gabyara237@gmail.com>
2025-12-17 15:51:30 -0800

    feat: implement follow route and add follow button on explore page

--- controllers/follows.js
+++ controllers/follows.js
@@ -1,6 +1,30 @@
 const express = require('express');
-
 const router = express.Router();
 
+const User = require('../models/user.js');
+const Follow = require('../models/follow.js');
+
+router.post('/:userId',async (req,res)=>{
+    try{
+        const followerId = req.session.user._id;
+        const followingId= req.params.userId;
+
+        if(followerId.toString() === followingId.toString()){
+            return res.redirect(`/users/${followingId}`)
+        }
+
+        await Follow.create({
+            follower: followerId,
+            following: followingId,
+        });
+
+        res.redirect(`/users/${followingId}`);
+
+    }catch (error){
+        console.log(error);
+        res.redirect('/');
+    }
+
+})
 
 module.exports= router;

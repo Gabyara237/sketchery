3c44e0364993fa8f20a71a113b64865910c00f1d
Gabriela Araujo <gabyara237@gmail.com>
2025-12-17 21:49:00 -0800

    feat: display follower and following counts on user profiles

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const {Artwork} = require('../models/artwork.js');
+const Follow = require('../models/follow.js');
 
 router.get('/:userId',async(req,res)=>{
     const userProfile = await User.findById(req.params.userId);
     const allArtworks = await Artwork.find({owner:req.params.userId});
-    res.render("users/show.ejs", {allArtworks, userProfile});
+
+    const followers = await Follow.find({following:req.session.user._id});
+    const following = await Follow.find({follower:req.params.userId});
+
+    res.render("users/show.ejs", {allArtworks, userProfile, followers, following});
 })
 
 
